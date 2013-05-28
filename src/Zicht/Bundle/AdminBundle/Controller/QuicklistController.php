c95191219cf6e35ce30ba2b805f019d13471a096
Gerard van Helden <gerard@zicht.nl>
2013-05-28 15:56:55 +0000

    CS
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtAdminBundle/trunk@20089 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/AdminBundle/Controller/QuicklistController.php
+++ src/Zicht/Bundle/AdminBundle/Controller/QuicklistController.php
@@ -5,19 +5,27 @@
  */
 namespace Zicht\Bundle\AdminBundle\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use \Symfony\Component\HttpFoundation\JsonResponse;
+use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\HttpFoundation\Request;
+use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Controls a quick list for configured entities.
+ */
 class QuicklistController extends Controller
 {
     /**
+     * Displays a quick list control for jumping to entries registered in the quick list service
+     *
+     * @param Request $request
+     * @return array
+     *
      * @Template
      * @Route("quick-list")
      */
-    function quicklistAction(Request $request)
+    public function quicklistAction(Request $request)
     {
         $quicklist = $this->get('zicht_admin.quicklist');
         if ($request->get('repo') && $request->get('pattern')) {
