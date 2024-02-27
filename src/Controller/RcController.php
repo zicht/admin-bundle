1d639fc4e849530642fc046aea43a5c10cb0f134
Jochem Klaver <jochem@fabrique.nl>
2024-02-27 13:00:40 +0100

    Remove Sensio Framework Extra Bundle

--- src/Controller/RcController.php
+++ src/Controller/RcController.php
@@ -5,29 +5,30 @@
 
 namespace Zicht\Bundle\AdminBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment as TwigEnvironment;
 
 class RcController
 {
+    /** @var TwigEnvironment */
+    private $twig;
+
     /**
      * @param array $configs
      */
-    public function __construct($configs)
+    public function __construct(TwigEnvironment $twig, $configs)
     {
+        $this->twig = $twig;
         $this->configs = $configs;
     }
 
     /**
      * Renders all RC controls
      *
-     * @return array
-     *
-     * @Template("@ZichtAdmin/Rc/controls.html.twig")
+     * @return Response
      */
     public function controlsAction()
     {
-        return [
-            'configs' => $this->configs,
-        ];
+        return $this->twig->render('@ZichtAdmin/Rc/controls.html.twig', ['configs' => $this->configs]);
     }
 }
